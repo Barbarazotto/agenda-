98c6b77e940611b16dadf4835c31bd702ffe87a4
Barbarazotto <zotto.s.barbara@gmail.com>
2026-02-02 16:49:24 -0300

    style: trocando a font e para Ubuntu

--- teste.py
+++ teste.py
@@ -1,1 +1,8 @@
-print("Olá, mundo")
+from models.database import Database
+
+# Teste do gerenciador de contexto
+with Database('./data/tarefas.sqlite3') as db: 
+    db.executar(
+        'INSERT INTO tarefas (titulo_tarefa, data_conclusao) VALUES (?, ?);', ('Usar o geranciador de contexto', '2026-02-03')
+    
+    )
