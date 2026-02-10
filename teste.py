10b8fa70a6f1444bda5f915a5c3fd5ec371ee436
Barbarazotto <zotto.s.barbara@gmail.com>
2026-02-10 10:09:03 -0300

    fazendo alterações no no código

--- teste.py
+++ teste.py
@@ -1,8 +1,9 @@
 from models.database import Database
 
-# Teste do gerenciador de contexto
-with Database('./data/tarefas.sqlite3') as db: 
+#teste do gerenciador de contexto
+with Database('./data/tarefas.sqlite3') as db:
     db.executar(
-        'INSERT INTO tarefas (titulo_tarefa, data_conclusao) VALUES (?, ?);', ('Usar o geranciador de contexto', '2026-02-03')
-    
+        'INSERT INTO tarefas (titulo_tarefa, data_conclusao) VALUES (?, ?):',
+                 ('Usar o gerenciador de contexto', '2026-02-03')
+           
     )
